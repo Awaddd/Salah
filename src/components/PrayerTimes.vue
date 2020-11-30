97c48eb1c6fe1e41ff87ec10212003537c1699b5
awaddd <awadini97@gmail.com>
2020-11-30 22:09:22 +0000

    switched to 24 hour clock

--- src/components/PrayerTimes.vue
+++ src/components/PrayerTimes.vue
@@ -4,11 +4,11 @@
 
   <div style="display: grid; grid-template-columns: max-content max-content; justify-content: center; grid-gap: 5em; font-size: 1.3em;">
     <ul style="display: grid; justify-items: start;">
-      <li v-for="(name, key) in state.prayers" :key="key">{{name}}</li>
+      <li v-for="(prayerName, key) in state.prayers" :key="key">{{prayerName}}</li>
     </ul>
 
     <ul style="display: grid;">
-      <li v-for="(name, key) in state.prayerTimes" :key="key">{{name}}</li>
+      <li v-for="(prayerTime, key) in state.prayerTimes" :key="key">{{prayerTime}}</li>
     </ul>
   </div>
   
@@ -23,6 +23,7 @@
 import { apiKey } from '../../config.json';
 import { ref, reactive, toRefs, computed } from "vue";
 import dayjs from "dayjs";
+import convertTime from "convert-time";
 
 const state = reactive({
   prayerTimes: {}, 
@@ -51,13 +52,25 @@ fetch(`https://www.londonprayertimes.com/api/times/?format=json&key=${apiKey}&ye
 
     state.prayerTimes = computed(() => {
       let prayer = {};
+
       for (let prayerTime in data.times[today.value]) {
         if (state.prayers.includes(prayerTime)) {
           prayer[prayerTime] = data.times[today.value][prayerTime]
         }
       }
+
+      prayer.fajr = convertTime(prayer.fajr + 'am');
+      prayer.sunrise = convertTime(prayer.sunrise + 'am');
+
+      let dhuhrLongDate = dayjs(today.value + ' ' + prayer.dhuhr);
+      prayer.dhuhr = dhuhrLongDate.isBefore(today.value + ' 11:59') ? convertTime(prayer.dhuhr + 'am') : convertTime(prayer.dhuhr + 'pm');
+
+      prayer.asr = convertTime(prayer.asr + 'pm');
+      prayer.magrib = convertTime(prayer.magrib + 'pm');
+      prayer.isha = convertTime(prayer.isha + 'pm');
+
       return prayer;
-    })
+    });   
   });
 
 </script>
